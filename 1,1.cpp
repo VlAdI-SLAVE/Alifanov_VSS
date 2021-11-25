d4b0ee53afab0c4d21a355e187758b252ec59018
Алифанов Владислав <92756672+VlAdI-SLAVE@users.noreply.github.com>
2021-11-25 21:39:32 +0000

    Исправленная задача 1.1

--- 1,1.cpp
+++ 1,1.cpp
@@ -1,30 +1,29 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
 /**
-* \brief Расчёт значения а
-* \param x Значение x
-* \param y Значение y
-* \param z Значение z
-* \return Значение а
+* \brief Расчёт значения а.
+* \param x Значение x.
+* \param y Значение y.
+* \param z Значение z.
+* \return Значение а.
 */
-double GetFoundA(const double x, const double y);
+double GetA(const double x, const double y);
 
 /**
-* \brief Расчёт значения b
-* \param x Значение x
-* \param y Значение y
-* \param z Значение z
-* \return Значение b
+* \brief Расчёт значения b.
+* \param x Значение x.
+* \param y Значение y.
+* \param z Значение z.
+* \return Значение b.
 */
-double GetFoundB(const double x, const double y, const double z);
+double GetB(const double x, const double y, const double z);
 
 int main()
 {    
-	// объявление и инициализация констант x, y, z
+	// объявление и инициализация констант x, y, z.
 	const auto x = 1.825;
 	const auto y = 18.225;
 	const auto z = -3.298;
@@ -33,23 +32,20 @@ int main()
 	const auto a = GetFoundA(x, y);
 	const auto b = GetFoundB(x, y, z);
 
-	// вывод исходных данных, т.е. значения x, y, z
+	// вывод исходных данных, т.е. значения x, y, z.
 	cout << "x = " << x << ", y = " << y << ", z = " << z << "\n";
-	// вывод результатов вычислений
+	// вывод результатов вычислений.
 	cout << "a = " << a << ", b = " << b;
 
 	return 0;
 }
 
-//  функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
-double GetFoundA(const double x, const double y)
+double GetA(const double x, const double y)
 {
-	// pow(...) - возведение числа в степень
-	return pow(x, y / x) - pow(y / x, 1 / 3);
+	return pow(x, y / x) - pow(y / x, 1.0/3.0);
 }
 
-//  функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
-double GetFoundB(const double x, const double y, const double z)
+double GetB(const double x, const double y, const double z)
 {
 	return (y - x) + ((y - z) / (y - x)) / (3 + ((z * z) / 5));
 }
