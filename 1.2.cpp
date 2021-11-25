8886fed21a9a79d41fa55f074adfcb2900016f41
Алифанов Владислав <92756672+VlAdI-SLAVE@users.noreply.github.com>
2021-11-25 21:47:27 +0000

    Исправленная задача 1.2

--- 1.2.cpp
+++ 1.2.cpp
@@ -1,66 +1,60 @@
-#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 
 using namespace std;
-/*
-* \brief Вычисляет площадь грани куба
-* \param edge Длина ребра куба
-* \return Площадь грани куба
-*/
 
+/*
+* \brief Вычисляет площадь грани куба.
+* \param edge Длина ребра куба.
+* \return Площадь грани куба.
+*/
 double GetArea(double edge);
 
 /*
-* \brief Вычисляет площадь полной поверхности куба
-* \param edge Длина ребра куба
-* \return Площадь полной поверхности куба
+* \brief Вычисляет площадь полной поверхности куба.
+* \param edge Длина ребра куба.
+* \return Площадь полной поверхности куба.
 */
-
 double GetSurfaceArea(double edge);
 
 /*
-* \brief Вычисляет объем куба
-* \param edge Длина ребра куба
-* \return Объем куба
+* \brief Вычисляет объем куба.
+* \param edge Длина ребра куба.
+* \return Объем куба.
 */
-
 double GetVolume(double edge);
 
 int main()
 {
-	// объявление переменной edge типа double
+	// объявление переменной edge типа double.
 	double edge;
-	// вывод вспомогательной строки в консоль
+	// вывод вспомогательной строки в консоль.
 	cout << "Cube edge length: ";
-	// присваивание edge значение, введенного с консоли
+	// присваивание edge значение, введенного с консоли.
 	cin >> edge;
 
-	// объявление констант area, SurfaceArea, volume, которым присвается значения функций  GetArea(edge), GetSurfaceArea(edge), GetVolume(edge) соответственно.
+	// объявление констант area, surfaceArea, volume, которым присвается значения функций  GetArea(edge), GetSurfaceArea(edge), GetVolume(edge) соответственно.
 	const double area = GetArea(edge);
-	const double SurfaceArea = GetSurfaceArea(edge);
+	const double surfaceArea = GetSurfaceArea(edge);
 	const double volume = GetVolume(edge);
 
-	// вывод результатов
+	// вывод результатов.
 	cout << "Area of the cube face: " << area << ", The total surface area of the cube: " << SurfaceArea << ", Cube volume: " << volume;
 
 	return 0;
 }
 
-// функция, которая принимает в качестве аргумента значение типа double и возвращает значение типа double.
-double GetArea(double edge)
+double GetArea(const double edge)
 {
 	return edge * edge;
 }
 
-// функция, которая принимает в качестве аргумента значение типа double и возвращает значение типа double.
-double GetSurfaceArea(double edge)
+double GetSurfaceArea(const double edge)
 {
 	return edge * edge * 6;
 }
 
-// функция, которая принимает в качестве аргумента значение типа double и возвращает значение типа double.
-double GetVolume(double edge)
+double GetVolume(const double edge)
 {
 	return edge * edge * edge;
 }
