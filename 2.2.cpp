0a3bda9a8e57c851177328ade1f79b09fa623fd1
Алифанов Владислав <92756672+VlAdI-SLAVE@users.noreply.github.com>
2021-11-25 21:57:09 +0000

    Исправленная задача 2.2

--- 2.2.cpp
+++ 2.2.cpp
@@ -1,4 +1,3 @@
-#define _USE_MATH_DEFINES 
 #include <iostream>
 #include <cmath>
 #include <iomanip>
@@ -23,27 +22,19 @@ double GetF2(const double x, const double a);
 
 int main()
 {
-	// объявление и инициализация констант a, top, bot типа double
     const double a = 2;
     const double top = 2;
-    const double bot = 1;
-	// объявление переменной x
+    const double b = 1;
     double x;
-	// вывод вспомогательной строки в консоль
     cout << "Enter x = "; 
-	// присваивание x значение, введенной с консоли
     cin >> x;
-	// объявление переменной result типа double
     double result;
 
-	// условный оператор, где условие x > top, если верно, то result присвоит значение функции GetF2(x, a)
-    if (x > top)
+     if (x > top)
     {
         result = GetF2(x, a);
     }
-
-	// условный оператор, где условие x < bot, если верно, то result присвоит значение 1, иначе присвоит result значение функции GetF1(x, a)
-    else if (x < bot)
+    else if (x < b)
     {
         result = 1;
     }
@@ -51,22 +42,16 @@ int main()
     {
         result = GetF1(x, a);
     }
-
-	// вывод result, setprecision(...) - задает точность значений с плавающей запятой.
     cout << "y = " << setprecision(15) << result;
     return 0;
 }
 
-// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
 double GetF1(const double x, const double a)
 {
-	// pow(...) - возведение числа в степень, log(...) - возвращает натуральный логарифм числа
-    return a * pow(x, 2 * log(x)); 
+	return a * pow(x, 2 * log(x)); 
 }
 
-// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
 double GetF2(const double x, const double a)
 {
-	// exp(...) - возведение экспоненты в степень, cos(...) - возвращает косинус числа
-    return exp(a * x) * cos(x);
+	 return exp(a * x) * cos(x);
 }
