c95d7d13543c35bf061d39b245977da195e931af
Алифанов Владислав <92756672+VlAdI-SLAVE@users.noreply.github.com>
2021-11-25 21:03:41 +0000

    Решена задача 2.1

--- 2.1.cpp
+++ 2.1.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 /**
  * \brief Расчет периметра прямоугольного треугольника
- * \param OneSide Длина первого катета
- * \param TwoSide Длина второго катета
+ * \param CatheterOne Длина первого катета
+ * \param CatheterTwo Длина второго катета
  * \return Периметр прямоугольного треугольника
  */
-double GetPerimeter(double OneSide, double TwoSide);
+double GetPerimeter(double CatheterOne, double CatheterTwo);
 
 /**
  * \brief Расчет площади прямоугольного треугольника
@@ -19,7 +19,7 @@ double GetPerimeter(double OneSide, double TwoSide);
  * \param CatheterTwo Длина второго катета
  * \return Площадь прямоугольного треугольника
  */
-double GetArea(double OneSide, double TwoSide);
+double GetArea(double CatheterOne, double CatheterTwo);
 
 /**
  * \brief Пользовательский выбор расчета  периметра (0) или площади (1)
@@ -28,29 +28,44 @@ enum class Choice { perimeter, area };
 
 int main()
 {
+	// объявление переменных CatheterOne, CatheterTwo типа double
 	double CatheterOne, CatheterTwo;
+	// вывод вспомогательной строки в консоль
 	cout << "Length of the first catheter: "; 
+	// присваивание CatheterOne значение, введенного с консоли
 	cin >> CatheterOne;
+	// вывод вспомогательной строки в консоль
 	cout << "Length of the second catheter: ";
+	// присваивание CatheterTwo значение, введенного с консоли
 	cin >> CatheterTwo;
 
+	// вывод вспомогательной строки в консоль
 	cout << "Enter an action on the triangle (perimeter = 0, area = 1) ";
+
+	// объявление переменной input типа int
 	int input;
+	// присваивание input значение, введенного с консоли
 	cin >> input;
 
+	// объявление и инициализация константы choice
 	const auto choice = static_cast<Choice>(input);
 
+	// оператор switch
 	switch (choice)
 	{
 		case Choice::perimeter:
         {
-           const double Perimeter = GetPerimeter(OneSide, TwoSide);
+		   // объявление и инициализация константы Perimeter, которой присвается значение результата вычисления функции GetPerimeter
+           const double Perimeter = GetPerimeter(CatheterOne, CatheterTwo);
+		   // вывод значения Perimeter
            cout << "Triangle perimeter = " << Perimeter;
            break;
         }
         case Choice::area:
         {
-           const double Area = GetArea(OneSide, TwoSide);
+		   // объявление и инициализация константы Area, которой присвается значение результата вычисления функции GetArea
+           const double Area = GetArea(CatheterOne, CatheterTwo);
+		   // вывод значения Area
            cout << "Area of the triangle = " << Area;
            break;
         }
@@ -58,12 +73,14 @@ int main()
 	return 0;
 }
 
-double GetPerimeter(double OneSide, double TwoSide)
+// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
+double GetPerimeter(double CatheterOne, double CatheterTwo)
 {
-	return OneSide + TwoSide + sqrt(OneSide * OneSide + TwoSide * TwoSide);
+	return CatheterOne + CatheterTwo + sqrt(CatheterOne * CatheterOne + CatheterTwo * CatheterTwo);
 }
 
-double GetArea(double OneSide, double TwoSide)
+// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
+double GetArea(double CatheterOne, double CatheterTwo)
 {
-	return (OneSide * TwoSide) / 2;
+	return CatheterOne * CatheterTwo / 2;
 }
