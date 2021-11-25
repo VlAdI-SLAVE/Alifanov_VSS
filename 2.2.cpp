e52378387298f2ae071cbc1cf8fdb19b1f9e42f1
Алифанов Владислав <92756672+VlAdI-SLAVE@users.noreply.github.com>
2021-11-25 21:06:30 +0000

    Решена задача 2.2

--- 2.2.cpp
+++ 2.2.cpp
@@ -6,55 +6,67 @@
 using namespace std;
 
 /**
- * \brief Функция вычисления для случая, x >= 1 и x <= 2
- * \param x Значение x
- * \param a Значение a
- * \return Значение функции
+ * \brief Функция расчета для случая, x >= 1 и x <= 2.
+ * \param x Значение x.
+ * \param a Значение a.
+ * \return Значение функции.
  */
-double GetFunctionOne(const double x, const double a);
+double GetF1(const double x, const double a);
 
 /**
- * \brief Функция вычисления для случая, когда x > 2
- * \param x Значение x
- * \param a Значение a
- * \return Значение функции
+ * \brief Функция расчета для случая, когда x > 2.
+ * \param x Значение x.
+ * \param a Значение a.
+ * \return Значение функции.
  */
-double GetFunctionTwo(const double x, const double a);
+double GetF2(const double x, const double a);
 
 int main()
 {
+	// объявление и инициализация констант a, top, bot типа double
     const double a = 2;
     const double top = 2;
     const double bot = 1;
+	// объявление переменной x
     double x;
+	// вывод вспомогательной строки в консоль
     cout << "Enter x = "; 
+	// присваивание x значение, введенной с консоли
     cin >> x;
+	// объявление переменной result типа double
     double result;
 
+	// условный оператор, где условие x > top, если верно, то result присвоит значение функции GetF2(x, a)
     if (x > top)
     {
-        result = GetFunctionTwo(x, a);
+        result = GetF2(x, a);
     }
 
+	// условный оператор, где условие x < bot, если верно, то result присвоит значение 1, иначе присвоит result значение функции GetF1(x, a)
     else if (x < bot)
     {
         result = 1;
     }
     else
     {
-        result = GetFunctionOne(x, a);
+        result = GetF1(x, a);
     }
 
+	// вывод result, setprecision(...) - задает точность значений с плавающей запятой.
     cout << "y = " << setprecision(15) << result;
     return 0;
 }
 
-double GetFunctionOne(const double x, const double a)
+// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
+double GetF1(const double x, const double a)
 {
-    return a * pow(x, 2 * log(x));
+	// pow(...) - возведение числа в степень, log(...) - возвращает натуральный логарифм числа
+    return a * pow(x, 2 * log(x)); 
 }
 
-double GetFunctionTwo(const double x, const double a)
+// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
+double GetF2(const double x, const double a)
 {
+	// exp(...) - возведение экспоненты в степень, cos(...) - возвращает косинус числа
     return exp(a * x) * cos(x);
 }
