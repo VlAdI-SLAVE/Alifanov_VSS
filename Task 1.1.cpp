5694726ce1cedbfc608db0ab61baa2afaee5def5
Алифанов Владислав <92756672+VlAdI-SLAVE@users.noreply.github.com>
2021-11-25 23:04:41 +0300

    Update Task 1.1.cpp

--- Task 1.1.cpp
+++ Task 1.1.cpp
@@ -5,44 +5,51 @@
 using namespace std;
 
 /**
-* \brief Вычисление значения а
+* \brief Расчёт значения а
 * \param x Значение x
 * \param y Значение y
 * \param z Значение z
 * \return Значение а
 */
-double GetA(const double x, const double y);
+double GetFoundA(const double x, const double y);
 
 /**
-* \brief Вычисление начение b
+* \brief Расчёт значения b
 * \param x Значение x
 * \param y Значение y
 * \param z Значение z
 * \return Значение b
 */
-double GetB(const double x, const double y, const double z);
+double GetFoundB(const double x, const double y, const double z);
 
 int main()
 {    
-	const double x = 1.825;
-	const double y = 18.225;
-	const double z = -3.298;
+	// объявление и инициализация констант x, y, z
+	const auto x = 1.825;
+	const auto y = 18.225;
+	const auto z = -3.298;
 
-	const double a = GetA(x, y);
-	const double b = GetB(x, y, z);
+	// объявление констант a, b, которым присвается значения функций GetFoundA(x, y) и GetFoundB(x, y, z) соответственно.
+	const auto a = GetFoundA(x, y);
+	const auto b = GetFoundB(x, y, z);
 
+	// вывод исходных данных, т.е. значения x, y, z
 	cout << "x = " << x << ", y = " << y << ", z = " << z << "\n";
+	// вывод результатов вычислений
 	cout << "a = " << a << ", b = " << b;
 
 	return 0;
 }
 
-double GetA(const double x, const double y)
+// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
+double GetFoundA(const double x, const double y)
 {
+	// pow(...) - возведение числа в степень
 	return pow(x, y / x) - pow(y / x, 1 / 3);
 }
 
-double GetB(const double x, const double y, const double z)
+// функция, которая принимает в качестве двух аргументов значения типа double и возвращает значение типа double.
+double GetFoundB(const double x, const double y, const double z)
 {
 	return (y - x) + ((y - z) / (y - x)) / (3 + ((z * z) / 5));
 }
